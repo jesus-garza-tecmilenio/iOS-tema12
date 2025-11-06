7c7955ff3edafb68c3a44145f3bf3ab62e8c2e6e
jesus-garza-gro <jesus.garza.gro@hotmail.com>
2025-11-06 17:03:33 -0600

    Initial Commit

---
+++ Tema12Swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tema12Swift
+//
+//  Created by JESUS GARZA on 06/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tema12Swift/Tema12SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tema12SwiftApp.swift
+//  Tema12Swift
+//
+//  Created by JESUS GARZA on 06/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Tema12SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
